bb6a0bcec17b870741b85a890c18a3ab8f78a18a
Chris K <Chris-SourceCode@outlook.com>
2021-07-19 22:33:06 +0100

    feat: add Card component

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = ({ name, email, id }) => (
+  <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
+    <img
+      alt="sweet looking cartoon kitten face"
+      src={`https://robohash.org/${id}?set=set4&size=200x200`}
+    />
+    <div>
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  </div>
+);
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Card from "./components/Card";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Card />
   </React.StrictMode>,
   document.getElementById("root")
 );
